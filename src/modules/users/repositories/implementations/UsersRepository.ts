07b49f54b791183743b2e0338422810aad1764ae
sabinorush <gu.sabino@hotmail.com>
2022-02-22 12:54:09 -0300

    First commit :tada:

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -19,7 +19,14 @@ class UsersRepository implements IUsersRepository {
   }
 
   create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
   }
 
   findById(id: string): User | undefined {
